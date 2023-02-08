714819f379f71245b7ad638d606b528d43788e88
Brodys1 <123720584+Brodys1@users.noreply.github.com>
2023-02-07 17:24:33 -0800

    lab 4

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def main():
     ##################################################
     # Comlete your code here
+    str = 'Python Programming'
+    sub1 = str[:6]
+    sub2 = str[-11:]
+    sub3 = sub2 + ' ' + sub1
+    print(f'{sub2}\n{sub1}\n{sub3}')
+    # print(f'{str[-11:]}\n{str[:6]}\n{str[-11:]} {str[:6]}')     #### Alternative only using one variable
     ##################################################
     pass
 
